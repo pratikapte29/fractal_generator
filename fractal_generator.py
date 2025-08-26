8e66b54acc3d2d9ea7db7e4f773b4b24a1cf13eb
pratikapte29 <pratikapte2911@gmail.com>
2025-08-26 21:49:37 +0530

    Update File structure

--- fractal_generator.py
+++ fractal_generator.py
@@ -17,60 +17,8 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 from PIL import Image
 import time
-
-# Embedded fractal generators (no external imports needed)
-class MandelbrotGenerator:
-    """Simple Mandelbrot generator without Numba"""
-    
-    @staticmethod
-    def generate(width=800, height=600, x_min=-2.0, x_max=2.0, 
-                 y_min=-2.0, y_max=2.0, max_iterations=100, **kwargs):
-        
-        x = np.linspace(x_min, x_max, width)
-        y = np.linspace(y_min, y_max, height)
-        X, Y = np.meshgrid(x, y)
-        C = X + 1j * Y
-        
-        result = np.zeros((height, width))
-        
-        for i in range(height):
-            for j in range(width):
-                c = C[i, j]
-                z = 0
-                for n in range(max_iterations):
-                    if abs(z) > 2:
-                        break
-                    z = z*z + c
-                result[i, j] = n
-        
-        return result
-
-class JuliaGenerator:
-    """Simple Julia set generator"""
-    
-    @staticmethod
-    def generate(width=800, height=600, x_min=-2.0, x_max=2.0, 
-                 y_min=-2.0, y_max=2.0, max_iterations=100,
-                 c_real=-0.7, c_imag=0.27015, **kwargs):
-        
-        x = np.linspace(x_min, x_max, width)
-        y = np.linspace(y_min, y_max, height)
-        X, Y = np.meshgrid(x, y)
-        Z = X + 1j * Y
-        
-        c = complex(c_real, c_imag)
-        result = np.zeros((height, width))
-        
-        for i in range(height):
-            for j in range(width):
-                z = Z[i, j]
-                for n in range(max_iterations):
-                    if abs(z) > 2:
-                        break
-                    z = z*z + c
-                result[i, j] = n
-        
-        return result
+from fractals.mandelbrot import MandelbrotGenerator
+from fractals.julia import JuliaGenerator
 
 class TestPatternGenerator:
     """Simple test pattern for debugging"""
--- fractals/julia.py
+++ fractals/julia.py
@@ -1,27 +1,24 @@
-import numpy as np
-# from numba import jit
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+import numpy as np 
 
-class MandelbrotGenerator:
-    """Simple Mandelbrot generator without Numba"""
+class JuliaGenerator:
+    """Simple Julia set generator"""
     
     @staticmethod
     def generate(width=800, height=600, x_min=-2.0, x_max=2.0, 
-                 y_min=-2.0, y_max=2.0, max_iterations=100, **kwargs):
+                 y_min=-2.0, y_max=2.0, max_iterations=100,
+                 c_real=-0.7, c_imag=0.27015, **kwargs):
         
         x = np.linspace(x_min, x_max, width)
         y = np.linspace(y_min, y_max, height)
         X, Y = np.meshgrid(x, y)
-        C = X + 1j * Y
+        Z = X + 1j * Y
         
+        c = complex(c_real, c_imag)
         result = np.zeros((height, width))
         
         for i in range(height):
             for j in range(width):
-                c = C[i, j]
-                z = 0
+                z = Z[i, j]
                 for n in range(max_iterations):
                     if abs(z) > 2:
                         break
